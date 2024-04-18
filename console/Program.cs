cf0bff9b635d5ac108a7623208fae6a76ea4e735
yanglijie <yanglj@csgiiot.com>
2024-04-18 16:50:21 +0800

    Test提交

---
+++ console/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using model;
+
+Console.WriteLine("Hello, World!");
+Class1 class1 = new Class1();
+class1.ts();
---
+++ model/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace model
+{
+    public class Class1
+    {
+        public void ts() 
+        { }
+    }
+}
